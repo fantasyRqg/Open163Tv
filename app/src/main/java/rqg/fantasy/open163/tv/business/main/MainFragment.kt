a8ba647bbb7353dda37374e4a7b8a259920379ac
rqg <ranqingguo90@qq.com>
2017-08-30 09:32:02 +0800

    fix keyboard focus

--- app/src/main/java/rqg/fantasy/open163/tv/business/main/MainFragment.kt
+++ app/src/main/java/rqg/fantasy/open163/tv/business/main/MainFragment.kt
@@ -7,6 +7,9 @@ import android.content.Context
 import android.content.Loader
 import android.os.Bundle
 import android.support.v17.leanback.app.BrowseFragment
+import android.support.v17.leanback.widget.ArrayObjectAdapter
+import android.support.v17.leanback.widget.HeaderItem
+import android.support.v17.leanback.widget.ListRowPresenter
 import rqg.fantasy.open163.tv.model.MovieItem
 
 /**
@@ -16,13 +19,36 @@ typealias CourseDataSet = Map<String, MutableList<MovieItem>>
 
 class MainFragment : BrowseFragment(), LoaderManager.LoaderCallbacks<CourseDataSet> {
 
+    private var mCategoryRowAdapter: ArrayObjectAdapter = ArrayObjectAdapter(ListRowPresenter())
+
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        loaderManager.initLoader<CourseDataSet>(0, null, null)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        adapter = mCategoryRowAdapter
+    }
+
+
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<CourseDataSet> {
         return DataLoader(activity)
     }
 
     override fun onLoadFinished(loader: Loader<CourseDataSet>?, data: CourseDataSet?) {
+        if (data == null)
+            return
 
 
+        mCategoryRowAdapter.clear()
+
+        for (ckey in data.keys) {
+            val header = HeaderItem(ckey)
+        }
     }
 
     override fun onLoaderReset(loader: Loader<CourseDataSet>?) {
